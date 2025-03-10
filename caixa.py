69b7a33d372f58f5e300bd96fd74539089d0e7bb
Kauê Sales <kahbyte@gmail.com>
2025-03-10 01:04:58 -0300

    adding output example

--- caixa.py
+++
@@ -1,7 +0,0 @@
-class Caixa:
-    def __init__(self, peso, pilha=None):
-        self.peso = peso  # Peso da caixa
-        self.pilha = pilha  # Referência à pilha onde a caixa está
-
-    def __repr__(self):
-        return f"Caixa({self.peso}kg)"
